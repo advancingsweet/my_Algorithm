dd95980137c3600582e7660a073663baf6e65161
Hongwei Tang <2428920861@qq.com>
2022-11-16 23:57:57 +0800

    Create Dijkstra（first）.cpp

---
+++ （C++版本）《数据结构与算法》基础算法/Dijkstra/Dijkstra（first）.cpp
@@ -0,0 +1,160 @@
+
+Dijkstra算法理解：
+1.构建图时，最好先画图，再写数据，一定要注意顶点都相连接了
+2.Dijkstra算法仅适合求一个顶点到其它顶点的最短距离，且其中权值不能有负数
+3.三部曲：
+-1.updata（第一个值先于循环处理）  
+-2.scan （寻找已有路径中最短路径）  
+-3.add（将到未添加顶点的最短路径添加（添加的是顶点））
+
+4.对于ts[vNum] 数组（the shortest length) : 
+-访问都是按照顶点之间的连接路径进行访问，所以所有访问的顶点都是在同一支路上。
+-ts[vNum]数值：表示的是从最初的顶点到某一点的距离，所以该距离有两种：1.从最初的顶点直接到达该顶点	2.从最初的顶点经过多个顶点到底该顶点。
+
+
+
+#include<iostream>
+#include<vector>
+#include<fstream>
+#include<iomanip>
+
+#define vNum 100
+#define INF 0x3f3f3f3f
+
+using namespace std;
+
+int ts[vNum] = {0};     //记录从一顶点到其余顶点的最短距离
+
+typedef struct Dijkstra{
+
+    int edge[vNum][vNum];   //邻接矩阵
+    int v;                  //顶点
+    int vis[vNum];       // 标记访问数组
+    int weight;         //权值
+    vector<int>path;    //动态数组记录最短路径
+}G;
+
+void InitGraph(G& g)
+{
+    fstream fs("input.txt",ios::in);
+    fs>>g.v;
+    for(int i=0;i<g.v;i++)
+    {
+        g.vis[i]=0;
+        for(int j=0;j<g.v;j++)
+        {
+            g.edge[i][j] = INF;
+        }
+    }
+    int v1,v2,w;
+    while(fs>>v1>>v2>>w,v1!=-1){
+        g.edge[v1][v2] = w;
+        g.edge[v2][v1] = w;
+    }
+    fs.close();
+}
+
+int Min(int *p,G g)  			//三部曲之二，scan最小值，并返回其下标
+{
+    int temp = INF,j;
+    for(int i=0;i<g.v;i++){
+        if(g.vis[i])  continue;		//一定要注意，访问过的顶点已被“添加”，所以不能再次添加，
+        if(p[i]<temp) {
+            temp = p[i];
+            j = i;
+        }
+    }
+    return j;
+}
+
+void Dijkstra(G &g,int st)    
+{
+    int count=1;				//第一个顶点在循环之外进行处理
+        int i;
+        g.vis[st]=1;     //若未被访问，则此刻被访问
+        for(i=0;i<g.v;i++)
+        {
+            ts[i] = g.edge[st][i]; 
+        }
+        g.path.push_back(st);
+        
+    while(count!=g.v){
+        count++;
+        int miN = Min(ts,g);
+        g.vis[miN] = 1;				
+        g.path.push_back(miN);      //三部曲之三，.add
+        int  sh = ts[miN];
+        
+         for(int i=0;i<g.v;i++)
+         {
+            if(ts[i]>g.edge[miN][i]+sh) 
+                ts[i] = g.edge[miN][i]+sh;		//三部曲之一： updata
+         }   
+        
+    }
+    ts[st] = 0;
+}
+
+void Display(G g)
+{
+for(int i=0;i<g.v;i++)
+    {
+        for(int j=0;j<g.v;j++)
+        {
+            cout<<setw(14)<<g.edge[i][j];
+        }
+        cout<<endl;    
+    }
+    cout<<"最短距离 : "<<endl;
+    for(int i=0;i<g.v;i++)
+    {
+        cout<<"["<<i<<"]"<<" : "<<ts[i]<<endl;
+    }
+    vector<int>::iterator it;   //迭代器
+    cout<<endl<<"路径为 : "<<endl;
+    for( it = g.path.begin();it < g.path.end();it++)
+    {
+        cout<<*it<<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    G g;
+    InitGraph(g);
+    Dijkstra(g,6);
+    Display(g);
+}
+
+/*
+#input.txt:
+7
+1 2 49
+2 3 23
+1 3 12
+1 4 7
+4 3 62
+1 0 23
+0 5 21
+3 5 16
+0 6 5
+6 3 11
+6 1 18
+5 4 33
+-1 -1 -1
+
+#result:
+最短距离 : 
+[0] : 5
+[1] : 18
+[2] : 34
+[3] : 11
+[4] : 25
+[5] : 26
+[6] : 0
+
+路径为 : 
+6 0 3 1 4 5 2 
+
+*/
